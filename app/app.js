4893998bde89ef4f7e1a6e13559755bf159b290a
Dharmik <135802029+DharmikDevops@users.noreply.github.com>
2026-06-24 11:11:23 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,15 @@
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(`
+        <h1>AWS ECS CI/CD Pipeline Project</h1>
+        <h2>Version 1</h2>
+        <h3>Deployed using Amazon ECS</h3>
+    `);
+});
+
+app.listen(3000, () => {
+    console.log("Application running on port 3000");
+});
